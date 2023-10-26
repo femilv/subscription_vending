176f895a29ba57e0ce7b576e7c0c5127d306d21c
Femi Ogunsakin <39163087+femilv@users.noreply.github.com>
2023-10-25 21:46:12 -0400

    Add files via upload

---
+++ subscription/header.md
@@ -0,0 +1,24 @@
+# Landing zone subscription submodule
+
+## Overview
+
+Creates a subscription alias, and optionally manages management group association for the resulting subscription.
+
+## Notes
+
+See [README.md](https://github.com/Azure/terraform-azurerm-lz-vending#readme) in the parent module for more information.
+
+## Example
+
+```terraform
+module "subscription" {
+  source  = "Azure/lz-vending/azurerm/modules/subscription"
+  version = "<version>" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  subscription_alias_billing_scope       = "/providers/Microsoft.Billing/billingAccounts/1234567/enrollmentAccounts/123456"
+  subscription_alias_display_name        = "my-subscription-display-name"
+  subscription_alias_name                = "my-subscription-alias"
+  subscription_alias_workload            = "Production"
+  subscription_alias_management_group_id = "mymg"
+}
+```
---
+++ subscription/variables.tf
@@ -0,0 +1,165 @@
+variable "subscription_alias_enabled" {
+  type        = bool
+  description = <<DESCRIPTION
+Whether to create a new subscription using the subscription alias resource.
+
+If enabled, the following must also be supplied:
+
+- `subscription_alias_name`
+- `subscription_display_name`
+- `subscription_billing_scope`
+- `subscription_workload`
+
+Optionally, supply the following to enable the placement of the subscription into a management group:
+
+- `subscription_management_group_id`
+- `subscription_management_group_association_enabled`
+
+If disabled, supply the `subscription_id` variable to use an existing subscription instead.
+
+> **Note**: When the subscription is destroyed, this module will try to remove the NetworkWatcherRG resource group using `az cli`.
+> This requires the `az cli` tool be installed and authenticated.
+> If the command fails for any reason, the provider will attempt to cancel the subscription anyway.
+DESCRIPTION
+
+  default = false
+}
+
+variable "subscription_alias_name" {
+  type        = string
+  default     = ""
+  description = <<DESCRIPTION
+The name of the subscription alias.
+
+The string must be comprised of a-z, A-Z, 0-9, - and _.
+The maximum length is 63 characters.
+
+You may also supply an empty string if you do not want to create a new subscription alias.
+In this scenario, `subscription_enabled` should be set to `false` and `subscription_id` must be supplied.
+DESCRIPTION
+  validation {
+    condition     = can(regex("^$|^[a-zA-Z0-9-_]{1,63}$", var.subscription_alias_name))
+    error_message = "Valid characters are a-z, A-Z, 0-9, -, _."
+  }
+}
+
+variable "subscription_display_name" {
+  type        = string
+  default     = ""
+  description = <<DESCRIPTION
+The display name of the subscription alias.
+
+The string must be comprised of a-z, A-Z, 0-9, -, _ and space.
+The maximum length is 63 characters.
+
+You may also supply an empty string if you do not want to create a new subscription alias.
+In this scenario, `subscription_enabled` should be set to `false` and `subscription_id` must be supplied.
+DESCRIPTION
+  validation {
+    condition     = can(regex("^$|^[a-zA-Z0-9-_ ]{1,63}$", var.subscription_display_name))
+    error_message = "Valid characters are a-z, A-Z, 0-9, -, _, and space."
+  }
+}
+
+variable "subscription_billing_scope" {
+  type        = string
+  default     = ""
+  description = <<DESCRIPTION
+The billing scope for the new subscription alias.
+
+A valid billing scope starts with `/providers/Microsoft.Billing/billingAccounts/` and is case sensitive.
+
+E.g.
+
+- For CustomerLed and FieldLed, e.g. MCA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/invoiceSections/{invoiceSectionName}`
+- For PartnerLed, e.g. MPA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/customers/{customerName}`
+- For Legacy EA - `/providers/Microsoft.Billing/billingAccounts/{billingAccountName}/enrollmentAccounts/{enrollmentAccountName}`
+
+You may also supply an empty string if you do not want to create a new subscription alias.
+In this scenario, `subscription_enabled` should be set to `false` and `subscription_id` must be supplied.
+DESCRIPTION
+  validation {
+    condition     = can(regex("^$|^/providers/Microsoft.Billing/billingAccounts/.*$", var.subscription_billing_scope))
+    error_message = "A valid billing scope starts with /providers/Microsoft.Billing/billingAccounts/ and is case sensitive."
+  }
+}
+
+variable "subscription_workload" {
+  type        = string
+  default     = ""
+  description = <<DESCRIPTION
+The billing scope for the new subscription alias.
+
+The workload type can be either `Production` or `DevTest` and is case sensitive.
+
+You may also supply an empty string if you do not want to create a new subscription alias.
+In this scenario, `subscription_enabled` should be set to `false` and `subscription_id` must be supplied.
+DESCRIPTION
+  validation {
+    condition     = can(regex("^$|^(Production|DevTest)$", var.subscription_workload))
+    error_message = "The workload type can be either Production or DevTest and is case sensitive."
+  }
+}
+
+variable "subscription_management_group_id" {
+  type        = string
+  default     = ""
+  description = <<DESCRIPTION
+The destination management group ID for the new subscription.
+
+**Note:** Do not supply the display name.
+The management group ID forms part of the Azure resource ID. E.g.,
+`/providers/Microsoft.Management/managementGroups/{managementGroupId}`.
+DESCRIPTION
+  validation {
+    condition     = can(regex("^$|^[().a-zA-Z0-9_-]{1,90}$", var.subscription_management_group_id))
+    error_message = "The management group ID must be between 1 and 90 characters in length and formed of the following characters: a-z, A-Z, 0-9, -, _, (, ), and a period (.)."
+  }
+}
+
+variable "subscription_management_group_association_enabled" {
+  type        = bool
+  default     = false
+  description = <<DESCRIPTION
+Whether to create the `azurerm_management_group_subscription_association` resource.
+
+If enabled, the `subscription_management_group_id` must also be supplied.
+DESCRIPTION
+}
+
+variable "subscription_id" {
+  type        = string
+  description = <<DESCRIPTION
+DESCRIPTION
+  default     = ""
+  validation {
+    condition     = can(regex("^$|^[a-f\\d]{4}(?:[a-f\\d]{4}-){4}[a-f\\d]{12}$", var.subscription_id))
+    error_message = "Must be empty, or a GUID in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx. All letters must be lowercase."
+  }
+}
+
+variable "subscription_tags" {
+  type        = map(string)
+  description = <<DESCRIPTION
+A map of tags to assign to the newly created subscription.
+Only valid when `subsciption_alias_enabled` is set to `true`.
+
+Example value:
+
+```terraform
+subscription_tags = {
+  mytag  = "myvalue"
+  mytag2 = "myvalue2"
+}
+```
+DESCRIPTION
+  default     = {}
+}
+
+variable "subscription_use_azapi" {
+  type        = bool
+  default     = false
+  description = <<DESCRIPTION
+Whether to use the azapi_resource resource to create the subscription alias. This includes the subscription alias in the management group.
+DESCRIPTION
+}
